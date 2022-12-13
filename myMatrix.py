deb8a1c8ef8151f4ca09bc3c4d1e4f768e507b9f
aurelien.kremer <aurelien.kremer@epita.fr>
2022-12-13 17:35:48 +0100

    transpose, int multiplication, true str,true constructor added

--- myMatrix.py
+++ myMatrix.py
@@ -1,8 +1,15 @@
 class Matrix:
-    def __init__(self):
-        self.data = [[0,0,0],
-                     [0,0,0],
-                     [0,0,0]]
+    def __init__(self, row=3,collumn=3):
+        self.data=[]
+        for line in range(collumn):
+            row_list= []
+            for line in range(row):
+                row_list.append(0)
+            self.data.append(row_list)
+            
+        #self.data = [[0,0,0],
+                    # [0,0,0],
+                   #  [0,0,0]]
 
          
     def __len__(self):
@@ -16,7 +23,11 @@ class Matrix:
         return value
 
     def __str__(self):
-        return str(self.data)
+        result =""
+        for row in self.data:
+            result+=str(row)+"\n"
+        return result
+            
 
     def __add__(self,operand):
         for y in range(len(self.data)):
@@ -28,4 +39,22 @@ class Matrix:
         for y in range(len(self.data)):
             for x in range(len(self.data[0])):
                 self.data[y][x] -= (operand[y,x])
-        return self                                                         
+        return self
+
+    def __mul__(self,operand):
+        if type(operand)==int:
+            for i in range(len(self.data)):
+                for l in range(len(self.data[0])):
+                    print(i)
+                    print(l)
+                    self.data[i][l]*=operand
+        return self.data
+    
+
+    def transpose(self):
+        tmp = Matrix(len(self.data),len(self.data[0]))
+        for x in range(len(self.data[0])):
+            for y in range(len(self.data)):
+                tmp[x,y]=self.data[y][x]
+        return tmp
+                    
